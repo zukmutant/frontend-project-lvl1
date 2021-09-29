00a973262aeea4af95f866182356978d1f566987
zukmutant <lazutkin@gmail.com>
2021-09-29 21:56:36 +0300

    even bug fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,47 +1,44 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
-var name = readlineSync.question ('May I have your name? ');
+const name = readlineSync.question('May I have your name? ');
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${name} `);
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const getRandomInt = () => {
-    return Math.floor(Math.random() * 100);
-}
-
+const getRandomInt = () => Math.floor(Math.random() * 100);
 
 let n = 0;
 
-while (n < 3); {
-    const numberForUser = getRandomInt ();
-    console.log(`Question: ${numberForUser}`);
-    const userAnswer = readlineSync.question ('Your answer: ');
+while (n < 3) {
+  const numberForUser = getRandomInt();
+  console.log(`Question: ${numberForUser}`);
+  const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer !== 'yes' || userAnswer !== 'no') {
-        console.log(`${userAnswer} is wrong answer ;(.`)
-        console.log(`Let's try again, ${name}!`);
-
-    }
-
+  if (userAnswer === 'yes' || userAnswer === 'no') {
     if (numberForUser % 2 === 0 && userAnswer === 'yes') {
-        console.log(`Correct!`);
-        n +=1;
+      console.log('Correct!');
+      n += 1;
     } else if (numberForUser % 2 !== 0 && userAnswer === 'no') {
-        console.log(`Correct!`);
-        n +=1;
+      console.log('Correct!');
+      n += 1;
+    } else if (userAnswer === 'yes') {
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`)
+        console.log(`Let's try again, ${name}!`);
+        break;
     } else {
-        if (userAnswer === 'yes') {
-            console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'`)
-            console.log(`Let's try again, ${name}!`);
- 
-        } else if (userAnswer === 'no') {
-            console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'Yes'`)
-            console.log(`Let's try again, ${name}!`);
-
-
-        }
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'Yes'.`)
+        console.log(`Let's try again, ${name}!`);
+        break;
+      
     }
- console.log(`Congratulations, ${name}!`);
-}
+  } else { 
+    console.log(`${userAnswer} is wrong answer ;(`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+};
 
+if (n === 3) {
+console.log(`Congratulations, ${name}!`);
+};
